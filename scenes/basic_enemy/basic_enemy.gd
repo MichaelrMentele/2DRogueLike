abc1eadf4980a06418c7b2876fc28afeb9c4594e
michael mentele <michaelrmentele@gmail.com>
2023-10-21 20:24:55 -0500

    add-sword-hit

--- scenes/basic_enemy/basic_enemy.gd
+++ scenes/basic_enemy/basic_enemy.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 const MAX_SPEED = 75
 
 func _ready() -> void:
-	pass
+	$Area2D.area_entered.connect(on_area_entered)
 
 
 func _process(delta):
@@ -16,3 +16,7 @@ func get_direction_to_player():
 	var player_node = get_tree().get_first_node_in_group("player") as Node2D
 	if player_node != null:
 		return (player_node.global_position - global_position).normalized()
+
+
+func on_area_entered(other_area: Area2D):
+	queue_free()
